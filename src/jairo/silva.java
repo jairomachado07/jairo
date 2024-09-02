8b73a99b13d893059256c9ee9a84a23329e98c91
jairomachado07 <jr.hk22@gmail.com>
2024-09-02 20:30:41 -0300

    atualização dia 02/09

--- src/jairo/silva.java
+++ src/jairo/silva.java
@@ -3,12 +3,11 @@ package jairo;
 public class silva {
 
 	public silva() {
-		System.out.println("Oi");
+	
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Oi de novo");
 	}
 
 }
